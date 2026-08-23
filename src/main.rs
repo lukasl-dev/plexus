99ee9093e503ef4b3b198f05bf5f0c815124fd2a
lukasl-dev <me@lukasl.dev>
2026-08-23 04:26:33 +0200

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "plexus", about = "ToDo")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Analyse {
+        #[arg(long, default_value = ".")]
+        root: PathBuf,
+
+        #[arg(long)]
+        lsp_server: String,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Analyse { root, lsp_server } => {
+            println!("Analysing {}", root.display());
+        }
+    }
+}
